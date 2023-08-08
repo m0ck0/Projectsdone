ded8056981e759f1456772ff1ec92959eef74389
m0ck0 <51659738+m0ck0@users.noreply.github.com>
2023-08-08 20:07:52 -0300

    si

--- encoders/encoders.ino
+++ encoders/encoders.ino
@@ -1,4 +1,6 @@
 #include <Keypad.h>
+#include <MIDI.h>
+MIDI_CREATE_DEFAULT_INSTANCE();
 const byte ROWS = 3;
 const byte COLS = 4;
 const int SHUTTLE1 = 4;
@@ -12,18 +14,18 @@ int valores = 0;
 int jogValue = 0;
 int prevJogValue = 0;
 unsigned long last_run=0;
-
-char hexaKeys[ROWS][COLS] = {
+byte customKey = 12;
+byte hexaKeys[ROWS][COLS] = {
   {'9','7','4','6'},
-  {'A','5','2','B'},
-  {'8','5','3','1'}
-};
+  {'10','5','2','A'},
+  {'8','5','3','1'}};
 byte rowPins[ROWS] = {14, 15, 16};
 byte colPins[COLS] = {8, 9, 10, 11}; 
 Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
 void setup(){
-  Serial.begin(9600);
+  MIDI.begin();
+  Serial.begin(115200);
   pinMode(SHUTTLE1, INPUT_PULLUP);
   pinMode(SHUTTLE2, INPUT_PULLUP);
   pinMode(SHUTTLE3, INPUT_PULLUP);
@@ -38,23 +40,20 @@ void loop(){
   prevShuttleValue = shuttleValue;
   bool jogChange = (jogValue != prevJogValue);
   prevJogValue = jogValue;
-
   bitWrite(output, 0 , !digitalRead(SHUTTLE4));
   bitWrite(output, 1 , !digitalRead(SHUTTLE3));
   bitWrite(output, 2 , !digitalRead(SHUTTLE2));
   bitWrite(output, 3 , !digitalRead(SHUTTLE1));
-  convert();
-
-  char customKey = customKeypad.getKey();
-  if (shuttleChange | jogChange | customKey){
+  shuttleCode();
+  customKey = customKeypad.getKey();
+  if (customKey){midikey();}
+}
+/*  if (shuttleChange | jogChange){
     Serial.print("valor: ");
     Serial.print(valores);
     Serial.print(" Jog: ");
     Serial.print(jogValue);
-    Serial.print(" Key: ");
-    Serial.println(customKey);
-}}
-
+}*/
 void shaft_moved(){
   if (millis()-last_run>5){
     if (digitalRead(2)==1){
@@ -63,10 +62,9 @@ void shaft_moved(){
     if (digitalRead(2)==0){
       jogValue--; 
     last_run=millis();
-  }
-}}
+  }}}
 
-void convert() {
+void shuttleCode() {
   if (output == 137) {valores = 101;}
   if (output == 139) {valores = 102;}
   if (output == 143) {valores = 103;}
@@ -82,5 +80,30 @@ void convert() {
   if (output == 133) {valores = 112;}
   if (output == 135) {valores = 113;}
   if (output == 131) {valores = 114;}
-  if (output == 129) {valores = 115;}
-}
+  if (output == 129) {valores = 115;}}
+void midikey(){
+  if (customKey==49){MIDI.sendControlChange(1,127,1);}
+  if (customKey==50){MIDI.sendControlChange(2,127,1);}
+  if (customKey==51){MIDI.sendControlChange(3,127,1);}
+  if (customKey==52){MIDI.sendControlChange(4,127,1);}
+  if (customKey==53){MIDI.sendControlChange(5,127,1);}
+  if (customKey==54){MIDI.sendControlChange(6,127,1);}
+  if (customKey==55){MIDI.sendControlChange(7,127,1);}
+  if (customKey==56){MIDI.sendControlChange(8,127,1);}
+  if (customKey==57){MIDI.sendControlChange(9,127,1);}
+  if (customKey==48){MIDI.sendControlChange(10,127,1);}
+  if (customKey==65){MIDI.sendControlChange(11,127,1);}
+  }
+ /* void midikey(){
+  if (customKey==49){MIDI.sendNoteOn(60,127,1);Serial.println("Freeze");}
+  if (customKey==50){MIDI.sendNoteOn(61,127,1);Serial.println("Index");}
+  if (customKey==51){MIDI.sendNoteOn(62,127,1);Serial.println("Mark");}
+  if (customKey==52){MIDI.sendNoteOn(63,127,1);Serial.println("Erase");}
+  if (customKey==53){MIDI.sendNoteOn(64,127,1);Serial.println("Stop");}
+  if (customKey==54){MIDI.sendNoteOn(65,127,1);Serial.println("Rec");}
+  if (customKey==55){MIDI.sendNoteOn(66,127,1);Serial.println("Pause");}
+  if (customKey==56){MIDI.sendNoteOn(67,127,1);Serial.println("Rew");}
+  if (customKey==57){MIDI.sendNoteOn(68,127,1);Serial.println("Play");}
+  if (customKey==48){MIDI.sendNoteOn(69,127,1);Serial.println("FFwd");}
+  if (customKey==65){MIDI.sendNoteOn(70,127,1);Serial.println("J/S");}
+  }*/
