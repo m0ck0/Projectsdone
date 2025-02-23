dc77dfce04b90a5a016d1037393912390421ddc2
m0ck0 <51659738+m0ck0@users.noreply.github.com>
2025-02-23 17:44:44 -0300

    librebreathe

--- demoncore/demoncore.ino
+++ demoncore/demoncore.ino
@@ -1,61 +1,43 @@
+#include <Breathe.h>
+Breathe Breathe;
 byte LED1 = 10; 
 byte LED2 = 11; 
 byte hallPin = A3; 
-int LEDs = LOW;
-int brightness = 0;  // how bright the LED is
-int fadeAmount = 5;  // how many points to fade the LED by
 const int offset = 532;
 float value;
 
 unsigned long previousMil = 0;
-unsigned long previousMillis = 0;
-const long interval = 20;
 const long inter = 200;
 
 void setup() {
   Serial.begin (9600);
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
-}
+  }
 
 void loop() {
 LED();
 HALL();
-breathe();
-}
+  }
 
 void LED(){
   if (value<3 && value>-3){
-      LEDs = LOW;
-    digitalWrite(LED1, LEDs);
-    analogWrite(LED2, brightness);
+    Breathe.set(LED1, LOW), 10;
+    Breathe.set(LED2, HIGH, 0.7, 10);
     }
   if (value>3 or value<-3){
-      LEDs = HIGH;
-    digitalWrite(LED1, LEDs);
-    digitalWrite(LED2, !LEDs);
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, LOW);
     }
-}
-
+  }
 
 void HALL(){ 
-unsigned long currentMil = millis();
-  if (currentMil - previousMil >= inter) {
-    previousMil = currentMil;
-  value = (analogRead(hallPin) - offset);
-  Serial.print("Value: ");
-  Serial.print(value, 1);
+  unsigned long currentMil = millis();
+    if (currentMil - previousMil >= inter) {
+      previousMil = currentMil;
+      value = (analogRead(hallPin) - offset);
+       Serial.print("Value: ");
+       Serial.println(value, 1);
+      }
   }
-}
 
-void breathe() {
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-//   analogWrite(LED1, brightness);
-    brightness = brightness + fadeAmount;
-      if (brightness <= 0 || brightness >= 255) {
-        fadeAmount = -fadeAmount;
-     }
-  }
-}
